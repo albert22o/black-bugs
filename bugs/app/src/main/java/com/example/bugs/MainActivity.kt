aee7b8afcee70daa51b23a8e63f0b98332f9eba6
Albert <albertoganesan5@gmail.com>
2025-10-31 13:04:01 +0700

    Основной проект

---
+++ bugs/app/src/main/java/com/example/bugs/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.bugs
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.example.bugs.managers.PlayerManager
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        PlayerManager.initialize(this)
+
+     setContentView(R.layout.activity_main)
+        val viewPager: ViewPager2 = findViewById(R.id.viewPager)
+        val tabLayout: TabLayout = findViewById(R.id.tabLayout)
+
+        val adapter = ViewPagerAdapter(this)
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = when (position) {
+                0 -> "Регистрация"
+                1 -> "Правила"
+                2 -> "Авторы"
+                3 -> "Настройки"
+                4 -> "Играть"
+                else -> "Таб $position"
+            }
+        }.attach()
+    }
+}
